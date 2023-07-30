c225a901474f84b4eaf5703d1707f794db96bb75
kinsoRick <kmasnaveev@gmail.com>
2023-07-30 19:13:30 +0300

    fix(workflow): actions-todos

--- src/index.tsx
+++ src/index.tsx
@@ -24,12 +24,7 @@ const root = createRoot(container);
 // Code that interface with external data have been separated into their own modules.
 // These includes:
 //
-// - `DataStore`
-// - `CodeRepository`
-// - `TaskManagementSystem`
-//
-// They can be mocked by creating a mock version using `__mocks__` folder.
-// https://jestjs.io/docs/en/manual-mocks
+// - another try
 root.render(
   <React.StrictMode>
     <Provider store={store}>
